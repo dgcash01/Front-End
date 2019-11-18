0c336efd637e865c250af3f8ef44a119888fe9cf
Javier <tsakasu88@gmail.com>
2019-11-18 18:41:03 -0500

    added form sign up for parent and volunteer also styled

--- disney-parent/src/App.js
+++ disney-parent/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import ParentSignUpForm from './components/SignUp/ParentSignUpForm'
+import VolunteerSignUpForm from './components/SignUp/VolunteerSignUpForm'
+import {Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" />
+      <Route path ="/volunteer-signup-form" component={VolunteerSignUpForm}/>
+      <Route path="/parent-signup-form" component={ParentSignUpForm}/>
     </div>
-  );
+  )
 }
-
+ 
 export default App;
